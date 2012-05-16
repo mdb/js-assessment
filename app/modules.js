d4ddb822064edd1e35ce3610bcfd7d9676c2cc9f
Mike Ball <mikedball@gmail.com>
2012-05-16 17:21:21 -0400

    Added fix for modules createModule method.

--- app/modules.js
+++ app/modules.js
@@ -2,8 +2,16 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function() {
   return {
-    createModule : function() {
-
+    createModule : function(greeting, name) {
+      var n = name,
+          g = greeting;
+      return {
+        name: n,
+        greeting: g,
+        sayIt: function () {
+          return g + ', ' + n;
+        }
+      }
     }
   };
 });
