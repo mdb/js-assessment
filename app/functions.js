88e0ddcc806ea3331261bdf001e4406e271379bf
Mike Ball <mikedball@gmail.com>
2012-05-16 17:13:00 -0400

    Added fix for argsAsArray method.

--- app/functions.js
+++ app/functions.js
@@ -3,7 +3,11 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     argsAsArray : function(arr) {
+      var greeting = arr[0],
+          name = arr[1],
+          punctuation = arr[2];
 
+      return greeting + ', ' + name + punctuation;
     },
 
     speak : function(fn, obj) {
