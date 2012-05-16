f72274e89603803761f30aed47e63cd92be480dd
Mike Ball <mikedball@gmail.com>
2012-05-16 17:05:41 -0400

    Added fix for async manipulateRemoteData method.

--- app/async.js
+++ app/async.js
@@ -10,7 +10,20 @@ define(function() {
     },
 
     manipulateRemoteData : function() {
+      var deferred = new $.Deferred();
 
+      $.get(url).then(function (result) {
+        var people = result.people,
+            peopleNames = [];
+
+        for (var i=0; i<people.length; i++) {
+          var n = people[i].name === 'Adam' ? 'Adem' : people[i].name;
+          peopleNames.push(n);
+        }
+        deferred.resolve(peopleNames.sort());
+      });
+
+      return deferred;
     }
   };
 });
