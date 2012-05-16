bcfdd3385b15e58df3e1b6dc79f106d414a71b12
Mike Ball <mikedball@gmail.com>
2012-05-16 17:04:54 -0400

    Added fix for async async method.

--- app/async.js
+++ app/async.js
@@ -1,7 +1,12 @@
 define(function() {
   return {
     async : function() {
+      var deferred = new $.Deferred(); 
+      setTimeout(function () {
+        deferred.resolve(true);
+      }, 10);
 
+      return deferred;
     },
 
     manipulateRemoteData : function() {
