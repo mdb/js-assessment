69752de7c0e4c5fef2286c68fa77d08f2f9bbb1a
Mike Ball <mikedball@gmail.com>
2012-05-16 16:49:06 -0400

    Added solution to array indexOf test.

--- app/arrays.js
+++ app/arrays.js
@@ -3,7 +3,14 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     indexOf : function(arr, item) {
-
+      var i,
+          arrLength = arr.length;
+      
+      for (i = 0; i <= arrLength; i++) {
+        if (arr[i] == item) {
+          return i;
+        }
+      }
     },
 
     sum : function(arr) {
